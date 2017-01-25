251c6661d88e14160f8d0e02f31b4fb56ca8b9ec
dbadb <dana.batali@gmail.com>
2017-01-25 13:52:36 -0800

    checkpoint

--- www/js/app.js
+++ www/js/app.js
@@ -2,9 +2,12 @@
 'use strict';
 var app = {
     mainContent: "#mainContent",
+    mainNav: "#mainNavList",
     homeHref: "#driver",
+    caret: " <span class='caret'></span>",
     openURL: null,
-    m_nettab: {},
+    currentPage: null,
+    varRegExp: /{\s*(\w+)\s*}/g, /* w is alphnum, s is space*/
 
     initialize: function() {
         document.addEventListener("DOMContentLoaded", this.onReady, false);
@@ -19,10 +22,14 @@ var app = {
     // onReady is invoked after all scripts have finished loading.
     onReady: function() { // event callback, 'this' not valid
         app.logMsg("deviceready");
-        if(window.location.hash) {
+        var initURL = app.homeHref;
+        if(window.location.hash)
+        {
             app.logMsg("using window.location.hash: " + window.location.hash);
+            initURL = window.location.hash;
             app.navigate(window.location.hash);
         }
+        app.navigate(initURL);
 
         // do this after first navigate
         global.onhashchange = function(arg) {
@@ -36,72 +43,101 @@ var app = {
 
     // navigate: is the primary entrypoint for switch views
     navigate: function(url) {
-        this.logMsg("navigate: " + url);
-        if(url === "")
+        var fields = url.split("?");
+        var params = (fields.length == 2) ? fields[1] : null;
+        var page = this.urlToPage(fields[0]);
+        if(this.currentPage !== page)
         {
-            this.loadPage("info", this.mainContent);
+            // this.logMsg("navigate: " + page);
+            this.currentPage = page;
+            this.loadPage(page, this.mainContent);
+            this.updateNav();
         }
-        else
-        if(!this.pageIsVisible("#mainNavList>li>a", url))
+        if(params !== null)
         {
-            this.loadPage(url.slice(1), this.mainContent);
+            this.updateParams(params);
         }
     },
 
+    updateNav: function() {
+        // this.logMsg("updateNav");
+        $("#mainNavList").find(".active").removeClass("active");
+        // find the parent of the <a> whose href endswith the current page.
+        $("#mainNavList a[href$='" + this.currentPage + "']").parent()
+                                                           .addClass("active");
+    },
+
+    updateParams: function(params) {
+    },
+
+    urlToPage: function(href) {
+        var i = href.lastIndexOf("/");
+        return href.slice(i+2); // eliminate hash
+    },
+
     loadPage: function(page, target) {
+        // this.logMsg("loadPage: " + page);
         var fileref = "/pages/" + page + ".html";
         this.sendGetRequest(fileref, function(html) {
             var targetElem = document.querySelector(target);
-            targetElem.innerHTML = html;
-            app.afterPageLoad(page);
+            app[page + "PageLoaded"](targetElem, html);
             // after a page is loaded we may need to refresh its connent
             // for example, the network tables table
         });
     },
 
-    afterPageLoad: function(page) {
-        switch(page) {
-        case "drivers":
-            
-            break;
-        case "nettab":
-            break;
-        case "developers":
-            break;
-        }
+    driverPageLoaded: function(targetElem, html) {
+        targetElem.innerHTML = html;
+        $(".selector").change(function() {
+            var value = $(this).val();
+            var key = $(this).attr("id");
+            var ntkey = "/SmartDashboard/" + key ;
+            NetworkTables.putValue(ntkey + "/selected", value);
+        });
     },
 
-    // pageIsVisible: given a selector url (ie: #developr), check
-    //     whether the associated page is already displayed.
-    pageIsVisible: function(selector, url) {
-        var mustload = false;
-        var itemlist = document.querySelectorAll(selector);
-        if(itemlist === null)
-        {
-            this.logMsg("ERROR Can't find items for: " + selector);
-        }
-        else
-        {
-            var i;
-            for(i=0;i<itemlist.length; i++)
-            {
-                var item = itemlist[i];
-                var classes = item.className;
-                if(item.href === url)
+    developerPageLoaded: function(targetElem, html) {
+        var map = {
+            loglevels:  "<option>DEBUG</option>"+
+                        "<option>INFO</option>"+
+                        "<option>NOTICE</option>"+
+                        "<option>WARNING</option>"+
+                        "<option>ERROR</option>",
+        };
+        targetElem.innerHTML = this.interpolate(html, map);
+        $(".selector").change(function() {
+            var value = $(this).val();
+            var key = $(this).attr("id");
+            var ntkey = "/SmartDashboard/" + key ;
+            NetworkTables.putValue(ntkey + "/selected", value);
+        });
+    },
+
+    nettabPageLoaded: function(targetElem, html) {
+        targetElem.innerHTML = html;
+        this.rebuildNetTab();
+    },
+
+    aboutPageLoaded: function(targetElem, html) {
+        targetElem.innerHTML = html;
+        $("#logo").animate({
+            width: "333px",
+          });
+    },
+
+    interpolate: function(body, map) {
+        var result = body.replace(this.varRegExp,
+            function(match, capture) {
+                var mm = map[capture];
+                if (typeof mm === "undefined") {
+                    return match;
+                }
+                else
                 {
-                    if(classes.indexOf(" active") === -1)
-                    {
-                        classes += " active";
-                        item.classname = classes;
-                        mustload = true;
-                    }
+                    return map[capture];
                 }
-                else {
-                    item.className = classes.replace(/active/g, "");
-                }
-            }
-        }
-        return mustload;
+            });
+        return result;
     },
 
     // network table callbacks ------------------------------------------------
@@ -125,10 +161,40 @@ var app = {
     },
 
     onNetTabChange: function(key, value, isNew) {
-        app.m_nettab[key] = value;
         app.logMsg("nettab entry changed: " + key +
                     " = " + value +
                     " new: " + isNew);
+
+        if(app.currentPage == "nettab")
+        {
+            // key thing here: we're using the various NetworkTable keys as
+            // the id of the elements that we're appending, for simplicity. However,
+            // the key names aren't always valid HTML identifiers, so we use
+            // the NetworkTables.keyToId() function to convert them appropriately
+            if (isNew)
+            {
+                var tr = $('<tr></tr>').appendTo($('#networktable > tbody:last'));
+                $('<td></td>').text(key).appendTo(tr);
+                $('<td></td>').attr('id', NetworkTables.keyToId(key))
+                               .text(value)
+                               .appendTo(tr);
+            }
+            else
+            {
+                // similarly, use keySelector to convert the key to a valid jQuery
+                // selector. This should work for class names also, not just for ids
+                $('#' + NetworkTables.keySelector(key)).text(value);
+            }
+        }
+    },
+
+    rebuildNetTab: function() {
+        var keys = NetworkTables.getKeys();
+        for(var i=0;i<keys.length; i++)
+        {
+            var key = keys[i];
+            app.onNetTabChange(key, NetworkTables.getValue(key), true);
+        }
     },
 
     // ajax utils -------------------------------------------------------------
