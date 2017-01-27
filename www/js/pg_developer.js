1e9de847cfa338da5ebe21f9cb5e3ec426d66527
dbadb <dana.batali@gmail.com>
2017-01-26 17:31:43 -0800

    Added support for graphing via flot package.

--- www/js/pg_developer.js
+++ www/js/pg_developer.js
@@ -4,7 +4,10 @@
 (function(global) {
 'use strict';
 var developer = {
+    randomData: [],
+
     pageLoaded: function(targetElem, html) {
+        var self = this;
         var map = {
             loglevels:  "<option>DEBUG</option>"+
                         "<option>INFO</option>"+
@@ -17,7 +20,7 @@ var developer = {
         // first initialize selectors from network tables.
         $(".selector").each(function() {
             var key = $(this).attr("id");
-            var ntkey = "/SmartDashboard/" + key ;
+            var ntkey = "/SmartDashboard/" + key;
             var val = NetworkTables.getValue(ntkey + "/selected");
             $(this).val(val);
         });
@@ -30,22 +33,76 @@ var developer = {
             NetworkTables.putValue(ntkey + "/selected", value);
         });
 
+        // build string.
+        var val = NetworkTables.getValue("/SmartDashboard/Build", "n/a");
+        $("#build").text("Robot sw build: " + val);
+
+        // dabble wth flot
+        var plot = $.plot("#randomPlot", [this.getRandomData()], {
+                series: {
+                    shadowSize: 0
+                },
+                yaxis: {
+                    min: 0,
+                    max: 100
+                },
+                xaxis: {
+                    show: false
+                }
+        });
+        function update() {
+            plot.setData([self.getRandomData()]);
+            plot.draw();
+            setTimeout(update, 30);
+        }
+        update();
+
         // dabble with justgage...
-        var self = this;
         this.imuHeadingGage = new JustGage({
             id: "imuHeadingGage",
             value: 67,
             min: -180,
             max: 180,
-            title: "IMU Heading"
+            title: "IMU Heading",
+            valueFontColor: "#888",
           });
         var changeGage = function() {
             var val = Math.floor(Math.random() * 360 - 180);
             self.imuHeadingGage.refresh(val);
-            window.setTimeout(changeGage, 1000);
+            window.setTimeout(changeGage, 2000);
         }
-        window.setTimeout(changeGage, 1000);
+        changeGage();
     },
+
+    getRandomData: function() {
+        if(this.randomData.length > 0)
+        {
+            this.randomData = this.randomData.slice(1);
+        }
+        while(this.randomData.length < 300)
+        {
+            var prev = this.randomData.length > 0 ?
+                        this.randomData[this.randomData.length-1] : 50;
+            var y = prev + Math.random() * 10 - 5;
+            if(y < 0)
+            {
+                y = 0;
+            }
+            else
+            if(y > 100)
+            {
+                y = 100;
+            }
+            this.randomData.push(y);
+        }
+        // Zip the generated y values with the x values
+        var res = [];
+        for(var i=0; i<this.randomData.length; i++)
+        {
+            res.push([i, this.randomData[i]]);
+        }
+        return res;
+    }
 };
 global.app.setPageHandler("developer", developer);
 })(window);
