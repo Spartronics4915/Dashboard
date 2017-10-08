c7c68ee09dca30bcd263d16353709c32ecaaa02d
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2017-10-07 23:34:21 -0700

    Add Control Manager.

---
+++ www/js/pg_controlmanager.js
@@ -0,0 +1,33 @@
+//
+// javascript page handler for controlmanager.html
+//
+(function(global) {
+  'use strict';
+  var controlManager = {
+    pageLoaded: function(targetElem, html) {
+      targetElem.innerHTML = html;
+    },
+
+    onNetTabChange: function(key, value, isNew) {
+      var keySuffix = "/SmartDashboard/ControlManager/";
+      if (key.substring(0, keySuffix.length) == keySuffix) {
+        var expressionName = key.substring(keySuffix.length,key.length);
+        var expressionID = $.escapeSelector(expressionName); // Needs >= v3.0 of jQuery
+        if (isNew) {
+          $("#expressionlist").append(
+            `<li class="col-sm-12 expression"><p class="col-sm-6">` + expressionName + `:&nbsp;</p>
+            <input id="` + expressionID + `" value="` + value + `" placeholder="Mathematical expression with variables" type="text" class="col-sm-6"/>
+            </li>`);
+
+          $("#"+expressionID).on("input", function(event) {
+            NetworkTables.putValue(key, $(this).val());
+          });
+        } else {
+          $("#"+expressionID).val(value);
+        }
+      }
+    },
+  };
+
+  global.app.setPageHandler("controlmanager", controlManager);
+})(window);
