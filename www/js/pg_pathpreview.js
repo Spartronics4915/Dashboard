c758ef011d13efbaf83ba300c468ab0dee63a050
Declan Freeman-Gleason <declanfreemangleason@gmail.com>
2018-02-25 19:21:21 -0800

    Get path previews fully working.

--- www/js/pg_pathpreview.js
+++ www/js/pg_pathpreview.js
@@ -4,16 +4,34 @@
 (function(global) {
 'use strict';
 
-var PivotColor = Object.freeze({ BLUE: "BLUE", RED: "RED"});
+var PivotColor = { BLUE: "BLUE", RED: "RED"};
+
+PivotColor.toColor = function(pc) {
+  if (pc === PivotColor.RED) {
+    return "#f93e3e";
+  } else {
+    return "#3e47f9";
+  }
+};
+
+PivotColor.inverse = function(pc) {
+  if (pc === PivotColor.RED) {
+    return PivotColor.BLUE;
+  } else {
+    return PivotColor.RED;
+  }
+};
+
+PivotColor = Object.freeze(PivotColor);
 
 class FieldConfiguration {
-  constructor(switchLeftColor, scaleLeftColor) {
-    this.switchLeftColor = switchLeftColor;
-    this.scaleLeftColor = scaleLeftColor;
+  constructor(switchTopColor, scaleTopColor) {
+    this.switchTopColor = switchTopColor;
+    this.scaleTopColor = scaleTopColor;
   }
 
   toString() {
-    return "Left switch is " + this.switchLeftColor + ", Left scale is " + this.scaleLeftColor;
+    return "Left switch is " + this.switchTopColor + ", Left scale is " + this.scaleTopColor;
   }
 }
 
@@ -28,14 +46,16 @@ const importData = new Map(); // Should be of type map[FieldConfiguration][]{fil
 const reader = new FileReader();
 var ctx;
 var canvas;
-var fieldImage;
+var fieldConfig = new FieldConfiguration(PivotColor.BLUE, PivotColor.BLUE);
+var curComposites;
 
+var fieldImage;
 const PATH_DATA_KEY = Object.freeze("autopaths");
 
-const TOP_SWITCH = new Path2D();
-const BOTTOM_SWITCH = new Path2D();
-const TOP_SCALE = new Path2D();
-const BOTTOM_SCALE = new Path2D();
+const SWITCH_TOP = new Path2D();
+const SWITCH_BOTTOM = new Path2D();
+const SCALE_TOP = new Path2D();
+const SCALE_BOTTOM = new Path2D();
 
 var pathpreview = {
     getFieldConfiguration: function() {
@@ -47,6 +67,10 @@ var pathpreview = {
       importData.set(new FieldConfiguration(PivotColor.BLUE, PivotColor.RED).toString(), []);
       importData.set(new FieldConfiguration(PivotColor.RED, PivotColor.RED).toString(), []);
       importData.set(new FieldConfiguration(PivotColor.BLUE, PivotColor.BLUE).toString(), []);
+    },
+
+    pivCol: function() {
+      return PivotColor.RED;
     },
 
     adjustCoordinatesForCanvas: function(canvas, mouseEvent){
@@ -62,31 +86,45 @@ var pathpreview = {
       var data = pathpreview.getPathData();
       var arrayLength = data.length;
       for (var i = 0; i < arrayLength; i++) {
-        var v = data[i];
+        let v = data[i];
+
+        // JSON can't store maps
+        v.composites = new Map(v.composites);
+
         $("#pathcon").prepend(`
           <div class="path">
             <canvas id="canvasbackground"></canvas>
             <h1 style="font-size: 150%;">${v.name}</h1>
-            <h5 style="font-size: 100%;"><a id="showpath" href="#pathpreview">Display path &raquo;</a></h5>
+            <h5 style="font-size: 100%;"><a id="showpath" href="#pathpreview" role="button">Display path &raquo;</a></h5>
           </div>
           `);
-          $("#showpath").click(function() {
-            $("#pathdisplay").show();
+        $("#showpath").click(function() {
+          $("#pathdisplay").show();
+          pathpreview.compositePaths(ctx, v.composites, fieldConfig.toString()).catch(err => {
+            console.error("Couldn't composite paths: " + err);
           });
-        // JSON can't store maps
-        v.composites = new Map(v.composites);
+          pathpreview.drawPivots(fieldConfig);
+        });
+
         $("#canvasbackground")[0].width = fieldImage.width;
         $("#canvasbackground")[0].height = fieldImage.height;
+
         pathpreview.compositePaths($("#canvasbackground")[0].getContext("2d"), v.composites, new FieldConfiguration(PivotColor.RED, PivotColor.BLUE).toString()).catch(err => {
-          console.error("Couldn't composite paths: " + err);
+          console.error("Couldn't composite paths for thumbnails: " + err);
         });
       }
     },
 
-    compositePaths: async function(c, composites, fieldConfig) { // This uses the display canvas for compositing
+    compositePaths: async function(c, composites, fieldConfig) {
+      curComposites = composites;
+
       c.clearRect(0, 0, c.canvas.width, c.canvas.height);
       c.drawImage(fieldImage, 0, 0);
       var array = composites.get(fieldConfig);
+      if (!(array instanceof Array)) {
+        console.error("Cannot composite an invalid field configuration");
+        return;
+      }
       var arrayLength = array.length;
       for (var i = 0; i < arrayLength; i++) {
         let image = await app.loadImage(array[i].file);
@@ -119,15 +157,40 @@ var pathpreview = {
       $("#name").val("");
     },
 
+    drawPivots: function(fieldConfig) {
+      ctx.restore();
+
+      ctx.strokeStyle = PivotColor.toColor(fieldConfig.scaleTopColor);
+      ctx.stroke(SCALE_TOP);
+      ctx.strokeStyle = PivotColor.toColor(PivotColor.inverse(fieldConfig.scaleTopColor));
+      ctx.stroke(SCALE_BOTTOM);
+
+      ctx.strokeStyle = PivotColor.toColor(fieldConfig.switchTopColor);
+      ctx.stroke(SWITCH_TOP);
+      ctx.strokeStyle = PivotColor.toColor(PivotColor.inverse(fieldConfig.switchTopColor));
+      ctx.stroke(SWITCH_BOTTOM);
+    },
+
     pageLoaded: async function(targetElem, html) {
         targetElem.innerHTML = html;
+
+        SWITCH_TOP.rect(376, 232, 126, 95);
+        SWITCH_BOTTOM.rect(376, 510, 126, 95);
+        SCALE_TOP.rect(783.5, 184, 126, 95);
+        SCALE_BOTTOM.rect(783.5, 556, 126, 95);
 
         ctx = $("#pathcanvas")[0].getContext("2d");
         canvas = $("#pathcanvas")[0];
         fieldImage = await app.loadImage("/images/field.png");
 
+        ctx.lineWidth = 20;
+
         canvas.width = fieldImage.width;
         canvas.height = fieldImage.height;
+
+        ctx.save();
+
+        pathpreview.drawPivots(fieldConfig);
 
         if (!app.storageAvailable("localStorage")) {
           alert("Local storage unavailable! Autonomous paths won't be saved or loaded. " + app.storageAvailable())
@@ -149,7 +212,7 @@ var pathpreview = {
                 <div class="checkbox col-sm-5">
                   <label><input type="checkbox" value="" id="compreversed">Reversed</label>
                 </div>
-                <a id="comprem" href="#pathpreview" class="col-sm-1"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
+                <a id="comprem" href="#pathpreview" role="button" class="col-sm-1"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></a>
               </div>
             </div>
             `);
@@ -218,6 +281,18 @@ var pathpreview = {
 
         $("#closedisplay").click(function() {
           $("#pathdisplay").hide();
+        });
+
+        $("#togswitch").click(function() {
+          fieldConfig.switchTopColor = PivotColor.inverse(fieldConfig.switchTopColor);
+          pathpreview.compositePaths(ctx, curComposites, fieldConfig.toString());
+          pathpreview.drawPivots(fieldConfig);
+        });
+
+        $("#togscale").click(function() {
+          fieldConfig.scaleTopColor = PivotColor.inverse(fieldConfig.scaleTopColor);
+          pathpreview.compositePaths(ctx, curComposites, fieldConfig.toString());
+          pathpreview.drawPivots(fieldConfig);
         });
     },
 };
